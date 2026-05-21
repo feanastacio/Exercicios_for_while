e4b33bdd538e6ee10945b3c967a7da39ea5138d8
feanastacio <fehanastaci008@gmail.com>
2026-05-21 19:10:24 -0300

    feat: Subindo alguns exercicios finalizados da aula 05

---
+++ exercicios.js
@@ -0,0 +1,83 @@
+// 1
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// 2
+// let numero = Number(prompt("Digite um número: "))
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${numero} x ${i} = ${numero * i}`);
+// }
+
+// 3
+// let n = Number(prompt("Digite um número: "))
+// let soma = 0
+// let conta = ""
+// for (let i = 1; i <= n; i++) {
+//     soma += i
+//     conta += i
+
+//     if (i < n) {
+//         conta += " + "
+//     }
+// }
+// console.log(`${conta} = ${soma}`)
+
+//Intermediario
+
+// 1
+// for (let i = 1; i <= 50; i++) {
+
+//     if (i % 2 == 0) {
+//         console.log(i)
+//     }
+
+// }
+
+// 2
+// let numeroSecreto = Math.floor(Math.random() * 100) + 1
+// let tentativa
+// do {
+//     tentativa = Number(prompt("Digite um número de 1 a 100:"))
+//     if (tentativa > numeroSecreto) {
+//         alert("O número secreto é menor!")
+//     } else if (tentativa < numeroSecreto) {
+//         alert("O número secreto é maior!")
+//     } else {
+//         alert("Parabéns! Você acertou!")
+//     }
+// } while (tentativa !== numeroSecreto)
+
+// 3 
+// let numero = Number(prompt("Digite um número:"))
+// do {
+//     console.log(numero)
+//     numero--
+// } while (numero >= 0)
+
+// 4
+// let senha = 1234
+// let resposta 
+
+// do {
+//     resposta = Number(prompt("Digite a senha para entrar no programa: "))
+//     if (resposta === senha) {
+//         alert("Senha correta, seja bem-vindo(a)!")
+//     } else {
+//         alert("Senha incorreta, tente novamente.")
+//     }
+// } while (resposta !== senha)
+
+// 5
+
+
+// Avançado
+
+// 2
+let numero = Number(prompt("Digite um número:"));
+
+for (let i = 1; i <= 10; i++) {
+
+    console.log(numero + " x " + i + " = " + (numero * i));
+
+}
